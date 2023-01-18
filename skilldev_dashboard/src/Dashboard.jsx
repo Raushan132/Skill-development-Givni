4435cfc9b906f1d6d76a0920b605d858423241d8
rajeevrk9901 <rajeevrk9901@gmail.com>
2023-01-18 16:11:07 +0530

    border fixed for logo

--- skilldev_dashboard/src/Dashboard.jsx
+++ skilldev_dashboard/src/Dashboard.jsx
@@ -10,7 +10,7 @@ const Dashboard = () => {
     return (
         <>
             {/* <Edit/>
-        <Sidebar/> */}
+            <Sidebar/> */}
             <div className='min-h-screen flex justify-center items-center px-24'>
 
                 <div className='flex w-full gap-8'>
@@ -21,13 +21,13 @@ const Dashboard = () => {
                                 <li className='flex items-center gap-2 cursor-pointer bg-red-400 px-6 py-2'><BiBookContent className='h-4'/>Content</li>
                                 <li className='flex items-center gap-2 cursor-pointer bg-red-400 px-6 py-2'><BsImageFill className='h-4'/>Image</li>
                             </ul>
-                            <div className='absolute w-48 h-48 bg-gray-200 opacity-70 bottom-12 rounded-full -left-8 border-8 border border-red-400'></div>
+                            <div className='absolute w-48 h-48 bg-gray-200 opacity-70 bottom-12 rounded-full -left-8 border-8 border-red-400'></div>
                             <img className='absolute w-48 bg-gray-200 opacity-70 bottom-20 rounded-full ' src={logo} alt="logo" />
                         </nav>
                     </div>
                     {/* Nav Container */}
                     <div className='flex py-12 bg-blue-200 w-full justify-center'>
-                        <h1 className="relative uppercase text-xl font-bold inline-block before:content-[''] before:rounded-full before:absolute before:top-7 before:left-0  before:w-full before:border before:border-2 before:bg-red-600 before:border-yellow-400 ">Dashboard V2.0</h1>
+                        <h1 className="relative uppercase text-xl font-bold inline-block before:content-[''] before:rounded-full before:absolute before:top-7 before:left-0  before:w-full before:border-2 before:bg-red-600 before:border-yellow-400 ">Dashboard V2.0</h1>
                         <div>
                             {/* use map here */}
 
