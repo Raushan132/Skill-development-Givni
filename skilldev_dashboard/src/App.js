4e9e318a745b93d2010d37ff661134cc1d6e7186
rajeevrk9901 <rajeevrk9901@gmail.com>
2023-01-19 15:47:45 +0530

    created component navpages editdetails

--- skilldev_dashboard/src/App.js
+++ skilldev_dashboard/src/App.js
@@ -1,13 +1,12 @@
 import Dashboard from './Dashboard';
 import './App.css';
-import Form from './component/Form';
+// import Form from './component/Form';
 
 function App() {
   return (
     <div className="App">
       <Dashboard/>
-      <Form/>
-
+      {/* <Form/> */}
     </div>
   );
 }
--- skilldev_dashboard/src/Dashboard.jsx
+++ skilldev_dashboard/src/Dashboard.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import Edit from './component/Edit'
 import Sidebar from './component/Sidebar'
+import { Routes, Route } from 'react-router-dom'
+import EditDetails from './component/EditDetails'
 
 
 
@@ -10,7 +12,11 @@ const Dashboard = () => {
             <div className='min-h-screen flex justify-center items-center px-24'>
                 <div className='flex w-full gap-8'>
                     <Sidebar />
-                    <Edit />
+                    <Routes>
+                        <Route to='/edit' element={<Edit />} />
+                        <Route to='/edit:_id' element={<EditDetails />} />
+                    </Routes>
+                    
                 </div>
             </div>
         </>
