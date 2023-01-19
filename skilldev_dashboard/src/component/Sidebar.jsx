f016b9f096d2902a38b3a6f0d9a2a4af77bf5003
rajeevrk9901 <rajeevrk9901@gmail.com>
2023-01-19 20:42:03 +0530

    Added EditNaDetails Component when you click on edit it will redirect on editnavpage

--- skilldev_dashboard/src/component/Sidebar.jsx
+++ skilldev_dashboard/src/component/Sidebar.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { BsSegmentedNav, BsImageFill } from "react-icons/bs";
 import { BiBookContent } from "react-icons/bi";
 import logo from "../images/logo.png"
+import { NavLink } from 'react-router-dom';
 
 const Sidebar = () => {
   return (
@@ -9,9 +10,9 @@ const Sidebar = () => {
     <div className='h-screen flex text-white text-bold'>
       <nav className='bg-red-200 flex items-start py-12 shadow-md relative'>
         <ul className=' flex flex-col gap-4'>
-          <li className='flex items-center gap-2 cursor-pointer bg-red-400 px-6 py-2 hover:bg-red-600'><BsSegmentedNav className='h-4' />Nav</li>
-          <li className='flex items-center gap-2 cursor-pointer bg-red-400 px-6 py-2 hover:bg-red-600'><BiBookContent className='h-4' />Content</li>
-          <li className='flex items-center gap-2 cursor-pointer bg-red-400 px-6 py-2 hover:bg-red-600'><BsImageFill className='h-4' />Image</li>
+          <NavLink to='/NavPage' className='flex items-center gap-2 cursor-pointer bg-red-400 px-6 py-2 hover:bg-red-600'><BsSegmentedNav className='h-4' />Nav</NavLink>
+          <NavLink className='flex items-center gap-2 cursor-pointer bg-red-400 px-6 py-2 hover:bg-red-600'><BiBookContent className='h-4' />Content</NavLink>
+          <NavLink className='flex items-center gap-2 cursor-pointer bg-red-400 px-6 py-2 hover:bg-red-600'><BsImageFill className='h-4' />Image</NavLink>
         </ul>
         <div className='absolute w-48 h-48 bg-gray-200 opacity-70 bottom-12 rounded-full -left-8 border-8 border-red-400'></div>
         <img className='absolute w-48 bg-gray-200 opacity-70 bottom-20 rounded-full ' src={logo} alt="logo" />
