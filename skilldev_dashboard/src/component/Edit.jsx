527083ea102b497133fbdb90bc072e67751b9f6f
rajeevrk9901 <rajeevrk9901@gmail.com>
2023-01-19 15:51:18 +0530

    added editdetails with id

--- skilldev_dashboard/src/component/Edit.jsx
+++ skilldev_dashboard/src/component/Edit.jsx
@@ -10,8 +10,9 @@ const Edit = () => {
       <h1 className="relative uppercase text-xl font-bold inline-block before:content-[''] before:rounded-full before:absolute before:top-7 before:left-0  before:w-full before:border-2 before:bg-red-600 before:border-yellow-400 ">Dashboard V2.0</h1>
       <div>
         {/* use map here */}
-        <NavPages/>
-
+         <NavPages/>
+         
+        
       </div>
     </div>
 
