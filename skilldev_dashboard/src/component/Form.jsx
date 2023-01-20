154b70b6c916eb255a461835215d9eead751cdb9
Raushan Kumar <raushankp75@gmail.com>
2023-01-20 13:05:29 +0530

    Adding post data functionality in the form version 2

--- skilldev_dashboard/src/component/Form.jsx
+++ skilldev_dashboard/src/component/Form.jsx
@@ -6,15 +6,15 @@ import { RxCross2 } from 'react-icons/rx';
 const Form = () => {
 
     // sending data to this url
-    const url = 'http://192.168.1.12:8081/api/header'
+    const url = 'http://192.168.1.12:8081/setheader'
 
     // adding data function
-    const [data, setData] = useState({ menu: '', submenu: '' })
-    const handleData = (e,i) => {
+    const [data, setData] = useState({ menu: ''})
+    const handleData = (e) => {
         const newData = { ...data }
         newData[e.target.id] = e.target.value;
         setData(newData)
-        // console.log(newData);
+        console.log(newData);
 
 
         // for new input field
@@ -29,23 +29,23 @@ const Form = () => {
     const handleAdd = (e) => {
         e.preventDefault();
         console.log(data);
-        Axios.post(url, { menu: data.menu, submenu: data.submenu }).then(res => { console.log(res.data); }).catch(err => { console.log('unsuccess'); })
+        Axios.post(url, { menu: data.menu, submenu: [{ nav:""}] }).then(res => { console.log(res.data); }).catch(err => { console.log('unsuccess'); })
     }
 
 
 
-    // Adding new field
-    const [fieldAdd, setFieldAdd] = useState([]);
-    const handleFieldAdd = () => {
-        const newFieldAdd=[...fieldAdd,[]]
-        setFieldAdd(newFieldAdd)
-    }
-    // Deleting new field
-    const handleDelete = (i) => {
-        const fieldDelete = [...fieldAdd]
-        fieldDelete.splice(i,1)
-        setFieldAdd(fieldDelete)
-    }
+    // // Adding new field
+    // const [fieldAdd, setFieldAdd] = useState([]);
+    // const handleFieldAdd = () => {
+    //     const newFieldAdd=[...fieldAdd,[]]
+    //     setFieldAdd(newFieldAdd)
+    // }
+    // // Deleting new field
+    // const handleDelete = (i) => {
+    //     const fieldDelete = [...fieldAdd]
+    //     fieldDelete.splice(i,1)
+    //     setFieldAdd(fieldDelete)
+    // }
 
     return (
         <div className='w-full max-w-xs my-40 mx-auto'>
@@ -61,16 +61,21 @@ const Form = () => {
                     <input type="text" id='submenu' value={data.submenu} onChange={handleData} className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline' placeholder='Enter data here' />
                 </div> */}
 
-                <div className='flex justify-between'>
+                {/* <div className='flex justify-between'>
                     <p className='text-sm pt-1'> Add More Submenu 👉 </p>
 
                     <div className="flex items-end justify-end">
                         <button onClick={()=>handleFieldAdd()} className='bg-blue-400 hover:bg-blue-500 text-white font-bold py-2 px-2 rounded focus:outline-none focus:shadow-outline mb-4'> <BiAddToQueue /> </button>
                     </div>
-                </div>
+                </div> */}
+
+                {/* <div className="mb-4">
+                <label className='block text-gray-700 text-sm font-bold mb-2'>Submenu</label>
+                <input type="text" id='submenu' value={data.submenu} onChange={handleData} className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mb-4' placeholder='Enter data here' />
+                </div> */}
 
                 {/* Map function for adding new field */}
-                {fieldAdd.map((data, i) => {
+                {/* {fieldAdd.map((data, i) => {
                         return(
                             <div className='flex justify-between'>
                             <input type="text" id='submenu' value={data.submenu} onChange={handleData} className='shadow appearance-none border rounded max-w-fit py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mb-4' placeholder='Enter data here' />
@@ -78,7 +83,7 @@ const Form = () => {
                             <button className=' bg-red-400 hover:bg-red-500 text-white mb-4 px-2 rounded-md' onClick={()=>handleDelete(i)}> <RxCross2/> </button>
                         </div>
                         )
-                    })}
+                    })} */}
 
                 <div className="flex items-center justify-between">
                     <button className='text-xl w-full bg-green-400 hover:bg-green-500 text-white font-bold py-1 px-3 rounded focus:outline-none focus:shadow-outline'>Add</button>
@@ -88,4 +93,4 @@ const Form = () => {
     )
 }
 
-// export default Form
+export default Form
